8a3c41b45e61122817cf82a40f391dfaa6779ce0
Abhishek Maurya <abhishekrmaurya2020@gmail.com>
2026-03-05 21:54:56 +0530

    second commit

---
+++ test/VehickleRegistry.js
@@ -0,0 +1,10 @@
+// const{ethers}=require('ethers')
+// const rpc=;
+// const publicket=;
+// const privatekey=;
+// const provider=new ethers provider.jsonrpcprovider(rpc);
+// const account=new ethers.Wallet(privatekey,provider);
+async function call(){
+
+}
+cALLgklgl
