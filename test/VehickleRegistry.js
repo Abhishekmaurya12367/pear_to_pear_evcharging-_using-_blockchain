7d7afad1015012e85be9161d00c3edab49124402
Abhishek Maurya <abhishekrmaurya2020@gmail.com>
2026-03-06 02:58:02 +0530

    second commit

--- test/VehickleRegistry.js
+++ test/VehickleRegistry.js
@@ -1,10 +1,24 @@
-// const{ethers}=require('ethers')
-// const rpc=;
-// const publicket=;
-// const privatekey=;
-// const provider=new ethers provider.jsonrpcprovider(rpc);
-// const account=new ethers.Wallet(privatekey,provider);
-async function call(){
+const{expect}=require("chai");
+const{ethers}=require("ethers");
+describe("vecleregster contract" ,function(){
+    let owner;
+    let registry;
+    let owner1;
+    let owner2;
+    beforeEach(async function(){
+        const[owner,user1,user2]=await ethers.getSigner();
+        const Registry=await ethers.getContractFactory('Userregistry');
+        registry=await Registry.deploy();
+        await registry.waitForDeployment;
+    });
+// first test check for deployment 
+it("the contract is deployed or not",async function(){
+    expect(registry.target).to.not.equal(0);
+})
+//second test case checking that the contract admin is deployed or not 
+it("the contract admin is deployed or not",async function(){
+    expect(registry.admin()).to.equal(owner.address);
+})
+it("")
 
-}
-cALLgklgl
+})
