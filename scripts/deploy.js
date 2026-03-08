1e941e528f0bf1b03471a45e75cae42623c8fe49
Abhishek Maurya <abhishekrmaurya2020@gmail.com>
2026-03-08 14:31:23 +0530

    second commit

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+async function main() {
+  const [deployer]=await ethers.getSigners()
+  const VehicleRegistry = await ethers.getContractFactory("Userregistry");
+
+  const registry = await VehicleRegistry.deploy();
+
+  await registry.waitForDeployment();
+
+  console.log("Contract deployed to:", await registry.getAddress());
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
